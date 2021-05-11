0ab1b4ec3cc365ba466330508653f13e33a3c504
matias685 <78141961+matias685@users.noreply.github.com>
2021-05-12 00:24:02 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#A020F0" },
+	[SchemeSel] = { "#eeeeee", "#327BD1" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
