57debc2b8c4541891444699bcd78d14aaa79c56a
matias685 <78141961+matias685@users.noreply.github.com>
2021-05-16 23:32:06 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 0;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Ubuntu Mono:style=Regular:size=14:antialias=true:autohint=true"
@@ -11,6 +12,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#327BD1" },
+    [SchemeSelHighlight] = { "#ffffff", "#005577" },
+    [SchemeNormHighlight] = { "#ffffff", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
